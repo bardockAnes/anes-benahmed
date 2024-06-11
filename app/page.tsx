8dc66db881e6658fa3a1897238a10879d9abf8e0
bardockanes <bardockanes@gmail.com>
2024-06-11 22:00:10 +0100

    fixing home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
 import '@/style/main.css'
 import TypingEffect from "@/components/TypingEffect";
+import { phrases } from "@/data/phrases";
+import About from "@/components/About";
 
 export default function Home() {
+
   return (
     <main>
-      <TypingEffect phrase={"”وَمَن يَتَّقِ اللَّهَ يَجْعَل لَّهُ مَخْرَجًا وَيَرْزُقْهُ مِنْ حَيْثُ لَا يَحْتَسِبُ ۚ وَمَن يَتَوَكَّلْ عَلَى اللَّهِ فَهُوَ حَسْبُهُ ۚ إِنَّ اللَّهَ بَالِغُ أَمْرِهِ ۚ قَدْ جَعَلَ اللَّهُ لِكُلِّ شَيْءٍ قَدْرًا“ (الطلاق: 2-3)."}/>
+      <TypingEffect phrases={phrases}/>
+      <About/>
     </main>
   );
 }
--- components/TypingEffect.jsx
+++ components/TypingEffect.jsx
@@ -1,9 +1,28 @@
-'use client'
+'use client';
 import React from 'react';
 import Typing from 'react-typing-effect';
+import { motion } from 'framer-motion';
 
-const TypingEffect = ({ phrase }) => {
-  return <Typing speed={50} text={phrase} className="typing-text"/>
+
+const TypingEffect = ({ phrases }) => {
+  return (
+    <motion.div 
+      className='typing-cont'
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <Typing
+        speed={60}
+        eraseSpeed={20}
+        eraseDelay={5000}
+        typingDelay={1000}
+        text={phrases}
+        className="typing-text"
+        cursorClassName="typing-cursor"
+      />
+    </motion.div>
+  );
 };
 
 export default TypingEffect;
